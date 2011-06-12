9c12a08c916f655ed4ddf7b097412630c6dd31ee
Jason Smith <jhs@iriscouch.com>
2011-06-12 13:26:46 +0000

    Firefox 3.6 support

--- test/browser/main.js
+++ test/browser/main.js
@@ -1,5 +1,21 @@
 window.process = { env: {} };
 
+if(!Object.keys)
+  Object.keys = function(o){
+    if (o !== Object(o))
+      throw new Error('Object.keys called on non-object');
+    var ret=[], p;
+    for (p in o)
+      if(Object.prototype.hasOwnProperty.call(o,p))
+        ret.push(p);
+    return ret;
+  }
+
+if(!Array.isArray)
+  Array.isArray = function(o) {
+    return Object.prototype.toString.call(o) === '[object Array]';
+  }
+
 define(['events', 'querystring', 'test/run'], function(events, querystring, test_runner) {
   $('#boot').html('Starting');
   // Set up some faux Node stuff.
