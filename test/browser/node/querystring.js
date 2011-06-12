c0c7b9bb1ce2fd528171c33f42f625682eccc247
Jason Smith <jhs@iriscouch.com>
2011-06-12 09:36:46 +0000

    querystring.parse implementation

--- test/browser/node/querystring.js
+++ test/browser/node/querystring.js
@@ -1,5 +1,19 @@
 define([], function() {
   var exports = {};
 
+  exports.parse = function(str) {
+    var result = {};
+
+    if(!str) return result;
+
+    var kvs = str.split('&');
+    kvs.forEach(function(pair) {
+      var both = pair.split('=');
+      result[both[0]] = both[1];
+    })
+
+    return result;
+  }
+
   return exports;
 })
